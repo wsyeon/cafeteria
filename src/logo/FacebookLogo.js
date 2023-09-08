cfeed57db90c768cad48590de658315f288562d8
C팀 <sywon2005@gmail.com>
2023-09-08 18:35:17 +0900

    fix

---
+++ src/logo/FacebookLogo.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+function FacebookLogo(props) {
+  return (
+    <svg
+      width={19}
+      height={19}
+      viewBox="0 0 19 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M18.5 9.555c0-5-4.03-9.055-9-9.055s-9 4.054-9 9.055c0 4.52 3.291 8.266 7.594 8.945v-6.328H5.809V9.555h2.285V7.56c0-2.27 1.343-3.523 3.4-3.523.984 0 2.014.177 2.014.177v2.228h-1.135c-1.118 0-1.467.698-1.467 1.414v1.699h2.496l-.399 2.617h-2.097V18.5c4.303-.68 7.594-4.425 7.594-8.945z"
+        fill="#000000"
+      />
+    </svg>
+  );
+}
+
+export default FacebookLogo;
