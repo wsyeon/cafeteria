66ac49eac4da1f937f904c38a53e5ceffa9e4b04
C팀 <sywon2005@gmail.com>
2023-09-12 20:41:32 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AppleSignIn from "react-apple-login";
 function App() {
   return (
     <div>
-      <h1>애플 로그인 예제</h1>
+      {/* <h1>애플 로그인 예제</h1>
       <AppleSignIn
         clientId="com.mise.cheryexam" // Apple 개발자 포털에서 생성한 클라이언트 ID로 변경
         redirectURI="https://www.cheryexam.com/login/api/oauth/callback/apple" // 콜백 URL 설정
@@ -13,7 +13,7 @@ function App() {
         responseMode="form_post"
         usePopup={false} // 팝업 사용 여부
         callback={(res) => console.log(res)}
-      />
+      /> */}
     </div>
   );
 }
